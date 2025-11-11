39c8fc6a795948164286526433c77f69fc83993a
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2025-11-11 21:26:52 +0200

    Create Solution.cpp

---
+++ 1472. Design Browser History/Solution.cpp
@@ -0,0 +1,43 @@
+class BrowserHistory {
+stack<string> backHistory;
+stack<string> forwardHistory;
+
+public:
+    BrowserHistory(string homepage) {
+        backHistory.push(homepage);
+        forwardHistory = stack<string>();
+    }
+    
+    void visit(string url) {
+        backHistory.push(url);
+        forwardHistory = stack<string>();
+    }
+    
+    string back(int steps) {
+        while(steps > 0 && backHistory.size() > 1) {
+            forwardHistory.push(backHistory.top());
+            backHistory.pop();
+            steps--;
+        }
+
+        return backHistory.top();
+    }
+    
+    string forward(int steps) {
+        while(steps > 0 && forwardHistory.size() > 0) {
+            backHistory.push(forwardHistory.top());
+            forwardHistory.pop();
+            steps--;
+        }
+
+        return backHistory.top();
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
